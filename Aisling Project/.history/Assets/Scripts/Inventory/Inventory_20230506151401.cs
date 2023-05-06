6f0a36fc595e3fc6738c7892e1aca9144489ac67
VnssJT <64807616+VnssJT@users.noreply.github.com>
2023-05-06 16:05:49 +0200

    memories technical
    - once all of the fragments are found, they are replaced with the Memory

---
+++ Aisling Project/.history/Assets/Scripts/Inventory/Inventory_20230506151401.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    
+    #region Singleton
+    public static Inventory instance;
+
+    private void Awake() {
+        if(instance != null){
+            Debug.LogWarning("More than one instance of Inventory found!");
+            Destroy(gameObject);
+        } 
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    #endregion
+    
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+    public int space = 10;
+    public List<Item> items = new List<Item>();
+    public bool Add(Item item){
+        
+        if(items.Count < space){
+            items.Add(item);
+            onItemChangedCallback?.Invoke();
+            return true;
+        } 
+
+        Debug.Log("Not enough room");
+        return false;
+        
+    }
+
+    public void remove (Item item){
+        items.Remove(item);
+    }
+
+    void deleteFragments(MemoryManager.MemoryIndex memoryID){
+        // search all of the fragments with this memoryID
+        foreach(Item item in items){
+            if(item is MemoryFragment && (item as MemoryFragment).MemoryID == memoryID){
+                
+            }
+        }
+    }
+}
---
+++ Aisling Project/.history/Assets/Scripts/MemoryManager_20230506150412.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MemoryManager : MonoBehaviour
+{
+
+    #region Singleton
+    public static MemoryManager instance;
+
+    private void Awake() {
+        if(instance != null){
+            Debug.LogWarning("More than one instance of Memory Manager found!");
+            Destroy(gameObject);
+        } 
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    #endregion
+
+    public enum MemoryIndex
+    {
+        VIOLET,
+        BLUE,
+        CYAN,
+        GREEN,
+        YELLOW,
+        ORANGE,
+        RED
+    }
+
+    public List<Memory> memories;
+    [HideInInspector] public List<bool> memoriesFound;
+
+    // EVENTS
+    #region Events
+    public delegate void MemoryAction();
+    // GREEN found
+    public static event MemoryAction OnGreenFound;
+    // ORANGE found
+    public static event MemoryAction OnOrangeFound;
+    // YELLOW found
+    public static event MemoryAction OnYellowFound;
+    // RED found
+    public static event MemoryAction OnRedFound;
+    #endregion
+
+    private void Start() {
+        // Reset fragments and memories found
+        memoriesFound.Clear();
+        foreach (Memory memory in memories)
+        {
+            memory.ResetFragments();
+            memoriesFound.Add(false);
+        }
+    }
+    public void AddFragment(MemoryIndex memoryID, int fragmentID){
+        Memory memory = memories[(int) memoryID];
+        memory.AddFragment(fragmentID);
+
+        // Check if it's the last fragment
+        foreach(bool fragmentFound in memory.fragmentsFound){
+            if(!fragmentFound){
+                return;
+            }
+        }
+
+        // All of the fragments were found
+        Debug.Log("MEMORY MANAGER: AddFragment_memories found count: " + memoriesFound.Count);
+        Debug.Log("MEMORY MANAGER: AddFragment_memoryID: " + memoryID);
+        memoriesFound[(int) memoryID] = true;
+        //Debug.Log("MEMORY MANAGER: memory " + memoryID + " found");
+
+        AddMemoryToInventory();
+
+        // Check if the memory is a gameplay trigger
+        switch (memoryID)
+        {
+            case MemoryIndex.GREEN:
+                OnGreenFound?.Invoke();
+                break;
+
+            case MemoryIndex.ORANGE:
+                OnOrangeFound?.Invoke();
+                break;
+
+            case MemoryIndex.YELLOW:
+                OnYellowFound?.Invoke();
+                break;
+
+            case MemoryIndex.RED:
+                OnRedFound?.Invoke();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+}
